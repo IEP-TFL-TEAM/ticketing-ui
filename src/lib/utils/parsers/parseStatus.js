b0d825dc50aa43c20c4d4ef19f75f909224c8b75
Josed Leavai <josedleavai@gmail.com>
2024-08-12 09:25:42 -0400

    [chore] updated status color-map

--- src/lib/utils/parsers/parseStatus.js
+++ src/lib/utils/parsers/parseStatus.js
@@ -1,10 +1,7 @@
 const statusMap = {
-	OPEN: 'badge variant-filled-secondary',
-	COMPLETE: 'badge variant-filled-success',
-	['IN PROGRESS']: 'badge variant-filled-warning',
 	CLOSED: 'badge variant-filled-error',
 	PENDING: 'badge variant-filled-secondary',
-	RESOLVED: 'badge variant-filled-surface'
+	APPROVED: 'badge variant-filled-success'
 };
 
 export const parseStatus = (status) => statusMap[status];
