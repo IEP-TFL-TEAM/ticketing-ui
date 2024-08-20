015ac62db739599cb1a1adc16236bc1ba4ffd6bd
Josed Leavai <josedleavai@gmail.com>
2024-08-20 03:31:35 -0400

    [update] updated toggletheme styling

--- src/lib/components/layout/ThemeSwitch.svelte
+++ src/lib/components/layout/ThemeSwitch.svelte
@@ -1,7 +1,11 @@
 <script>
 	import { onMount } from 'svelte';
-	import { modeCurrent, setModeCurrent, setModeUserPrefers } from '@skeletonlabs/skeleton';
-	import { IconSun, IconMoon } from '@tabler/icons-svelte';
+	import {
+		modeCurrent,
+		setModeCurrent,
+		setModeUserPrefers,
+		SlideToggle
+	} from '@skeletonlabs/skeleton';
 
 	function toggleTheme() {
 		$modeCurrent = !$modeCurrent;
@@ -14,13 +18,4 @@
 	});
 </script>
 
-<button
-	class=" bg-white text-black dark:text-white hover:bg-white hover:text-black dark:hover:text-white w-[4rem] h-[4rem] bg-opacity-80 backdrop-blur-[0.5rem] border dark:border-white border-black border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-[#252424] dark:hover:bg-gray-950/50 group"
-	on:click={() => toggleTheme()}
->
-	<svelte:component
-		this={$modeCurrent ? IconSun : IconMoon}
-		name={$modeCurrent ? IconSun : IconMoon}
-		class="w-[1.5rem] h-[1.5rem] group-hover:scale-125 transition-all"
-	/>
-</button>
+<SlideToggle name="slide" bind:checked={$modeCurrent} on:click={() => toggleTheme()} />
