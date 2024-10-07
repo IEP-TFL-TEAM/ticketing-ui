0c45d5217c85fcd2487f0bb41c2323456da30ea4
Josed Leavai <josedleavai@gmail.com>
2024-10-06 22:19:50 -0400

    [chore] updated view image modal

--- src/lib/components/modals/ViewImage.svelte
+++ src/lib/components/modals/ViewImage.svelte
@@ -7,19 +7,19 @@
 
 	const modalStore = getModalStore();
 	const imageSrc = $modalStore[0].meta.imageSrc;
-
-	const cButton = 'fixed top-4 right-4 z-50 font-bold shadow-xl';
-	const cImage =
-		'max-w-[55%] max-h-[55%] rounded-container-token shadow-xl transition duration-500 opacity-0';
 </script>
 
 {#if $modalStore[0]}
 	<button
-		class=" p-3 rounded-full border border-gray-400 hover:border-none hover:cursor-pointer text-white hover:bg-black/60 dark:hover:bg-white/20 transition-all {cButton}"
+		class=" p-3 rounded-full border border-gray-400 hover:border-none hover:cursor-pointer text-white hover:bg-black/60 dark:hover:bg-white/20 transition-all fixed top-4 right-4 z-50 font-bold shadow-xl"
 		on:click={parent.onClose}
 	>
 		<IconX size={25} />
 	</button>
 
-	<img use:lazyLoad={imageSrc} class={cImage + ' overflow-y-auto'} alt="_" />
+	<img
+		use:lazyLoad={imageSrc}
+		class="max-w-5xl max-h-[55rem] overflow-y-auto h-full rounded-container-token shadow-xl transition duration-500 opacity-0"
+		alt="_"
+	/>
 {/if}
