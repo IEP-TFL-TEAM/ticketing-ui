d69adaed5cd953394f022086156605ab783c8321
Josed Leavai <josedleavai@gmail.com>
2024-10-15 22:50:27 -0400

    [update] updated login page styling

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,6 +3,7 @@
 	import { goto } from '$app/navigation';
 	import { getToastStore } from '@skeletonlabs/skeleton';
 	import { TransparentLogo } from '$lib/assets';
+	import { IconLogin2 } from '@tabler/icons-svelte';
 
 	const toastStore = getToastStore();
 
@@ -21,7 +22,7 @@
 			toastStore.trigger({
 				message: error.error_description || error.message,
 				background: 'bg-[#252424] text-white',
-				classes: 'hover:scale-110 border-l-4 border-red-500 shadow-2xl transition',
+				classes: 'border-l-4 border-red-500 shadow-2xl transition',
 				timeout: 3000
 			});
 		}
@@ -31,75 +32,56 @@
 	$: if ($currentUser) {
 		goto('/');
 	}
-
-	const inputStyles =
-		'w-full bg-transparent text-white border-0 border-b-2 border-neutral-500 outline-0 focus:outline-none focus:ring-0 focus:shadow-none py-2 focus:border-tertiary-500 focus:border-b-2 transition-colors peer';
-	const labelStyles =
-		'absolute peer-focus:left-0 top-2 text-neutral-500 cursor-text peer-focus:-top-5 peer-focus:text-tertiary-500 font-bold transition-all';
 </script>
 
 <svelte:head>
 	<title>Login Page</title>
 </svelte:head>
 
-<div class="min-h-screen w-full flex flex-col items-center justify-center bg-[#252424]">
-	<div class="flex flex-col px-4 sm:px-6 md:px-8 lg:px-10 py-8 md:py-14 w-full max-w-md gap-y-10">
-		<div class="flex flex-col gap-y-4">
-			<div class="w-full flex justify-center">
-				<img src={TransparentLogo} alt="logo" />
-			</div>
-
-			<h1 class="h1 font-extrabold text-xl sm:text-2xl text-tertiary-500 text-center">
-				Welcome to NOC <br /> Ticketing
-			</h1>
+<div class="flex flex-col items-center justify-center w-full min-h-screen bg-neutral-50">
+	<div
+		class="flex flex-col w-full max-w-lg px-4 py-8 bg-white shadow-2xl sm:px-6 md:px-8 lg:px-10 rounded-none"
+	>
+		<div class="flex justify-center items-center">
+			<img src={TransparentLogo} class="" alt="_" />
 		</div>
 
-		<form class="flex flex-col gap-y-6 px-4" on:submit|preventDefault={() => handleLogin()}>
-			<div class="relative">
+		<div class="self-center text-xl font-bold text-center text-gray-800 uppercase sm:text-2xl mt-4">
+			NOC Ticketing Portal Login
+		</div>
+
+		<div class="relative h-px my-10 bg-gray-300">
+			<div class="absolute top-0 left-0 flex justify-center w-full -mt-2">
+				<span class="px-4 text-xs text-gray-500 uppercase bg-white"> Login With Email </span>
+			</div>
+		</div>
+		<form on:submit|preventDefault>
+			<div class="max-w-sm mx-auto">
 				<input
-					id="email"
-					type="email"
+					class="w-full px-8 py-4 text-sm font-medium text-black placeholder-gray-500 border border-gray-200 rounded-none bg-neutral-50 focus:outline-none focus:border-gray-400 focus:bg-white"
+					placeholder="Email"
 					bind:value={email}
+					type="email"
+					name="email"
 					required
-					class={inputStyles}
-					autocomplete="off"
 				/>
-				<label
-					for="email"
-					class={`${labelStyles} ${
-						!email || email.length === 0 ? 'left-2' : 'left-0 -top-5 text-tertiary-500'
-					}`}
-				>
-					{!email || email.length === 0 ? 'Email' : ''}
-				</label>
-			</div>
 
-			<div class="relative">
 				<input
-					id="password"
+					class="w-full px-8 py-4 mt-5 text-sm font-medium text-black placeholder-gray-500 border border-gray-200 rounded-none bg-neutral-50 focus:outline-none focus:border-gray-400 focus:bg-white"
 					type="password"
+					name="password"
 					bind:value={password}
+					placeholder="Password"
 					required
-					class={inputStyles}
-					autocomplete="off"
 				/>
-				<label
-					for="password"
-					class="{labelStyles} {!password || password.length === 0
-						? 'left-2'
-						: 'left-0 -top-5 text-tertiary-500'}"
-				>
-					{!password || password.length === 0 ? 'Password' : ''}
-				</label>
-			</div>
-
-			<div class="flex w-full mt-4">
 				<button
+					class="flex items-center justify-center w-full py-4 my-8 font-semibold tracking-wide text-white transition-all duration-300 ease-in-out rounded-none bg-primary-500 text-white-500 hover:bg-primary-700 focus:shadow-outline focus:outline-none gap-x-4"
 					type="submit"
-					class="btn rounded-full py-2 w-full font-bold bg-gradient-to-r from-primary-500 to-tertiary-500 text-white focus:outline-none"
+					on:click|preventDefault={handleLogin}
 					disabled={loading}
 				>
-					Login
+					<IconLogin2 class="w-6 h-6 -ml-2" />
+					<span> Login </span>
 				</button>
 			</div>
 		</form>
