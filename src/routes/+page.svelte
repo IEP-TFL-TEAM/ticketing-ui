7b10fede764e375b94d58781c8ab7d0329f6311c
Josed Leavai <josedleavai@gmail.com>
2024-11-07 11:01:13 -0500

    [new] added metabase to dashboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,4 @@
 <script>
-	import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
-
-	import StatusCard from '$lib/components/home/StatusCard.svelte';
-	import IncidentCharts from '$lib/components/home/IncidentCharts.svelte';
-	import RequestCharts from '$lib/components/home/RequestCharts.svelte';
-	import RoutineCharts from '$lib/components/home/RoutineCharts.svelte';
-
-	export let data;
-	$: ({ tickets, requests, routines } = data);
-
-	let tab = 'Incidents';
 </script>
 
 <svelte:head>
@@ -22,33 +11,14 @@
 		<p>Navigate through the system with options on the sidebar</p>
 	</div>
 
-	<div class="mt-4 2xl:w-1/2 w-full">
-		<RadioGroup rounded="rounded-none" background="bg-white dark:bg-[#252424]">
-			{#each ['Incidents', 'Change Requests', 'Routine Maintenance'] as item}
-				<RadioItem bind:group={tab} name="justify" value={item}>{item}</RadioItem>
-			{/each}
-		</RadioGroup>
-	</div>
-
-	<div class="flex flex-col w-full mt-8">
-		{#if tab === 'Incidents'}
-			<div class="flex flex-col w-full gap-10">
-				<StatusCard records={tickets} title="Incidents" />
-			</div>
-
-			<IncidentCharts {data} />
-		{:else if tab === 'Change Requests'}
-			<div class="flex flex-col w-full gap-10">
-				<StatusCard records={requests} title="Change Requests" />
-			</div>
-
-			<RequestCharts {data} />
-		{:else if tab === 'Routine Maintenance'}
-			<div class="flex flex-col w-full gap-10">
-				<StatusCard records={routines} title="Routine Maintenance" />
-			</div>
-
-			<RoutineCharts {data} />
-		{/if}
+	<div class="flex w-full h-[3000px]">
+		<!-- Change url accordingly to the deployed metabase instance -->
+		<iframe
+			title="Ticketing Dashboard"
+			src="http://localhost:3000/public/dashboard/c5f4cdd5-a7c3-4637-8016-0bfdd1f6630e"
+			frameborder="0"
+			allowtransparency
+			class="flex-grow h-full"
+		/>
 	</div>
 </div>
