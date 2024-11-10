c303a0a6c3bcb54d3dae9e96493f04c556f6375d
Josed Leavai <josedleavai@gmail.com>
2024-11-10 11:37:37 -0500

    [new] added production metabase dashboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,9 +13,12 @@
 
 	<div class="flex w-full h-[3000px]">
 		<!-- Change url accordingly to the deployed metabase instance -->
+
+		<!-- Josed's Dev Dashboard -->
+		<!-- src="http://localhost:3000/public/dashboard/c5f4cdd5-a7c3-4637-8016-0bfdd1f6630e" -->
 		<iframe
 			title="Ticketing Dashboard"
-			src="http://localhost:3000/public/dashboard/c5f4cdd5-a7c3-4637-8016-0bfdd1f6630e"
+			src="http://192.168.20.2:3001/public/dashboard/01737b4c-2560-449a-b8ea-90a3590a2f43"
 			frameborder="0"
 			allowtransparency
 			class="flex-grow h-full"
